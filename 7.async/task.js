29e75d31f96947f7df29fe9fb60ebeb82d50e040
SergozBy <sergozby@gmail.com>
2025-04-01 23:59:48 +0300

    Implementation

---
+++ 7.async/task.js
@@ -0,0 +1,75 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.inervalid = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсуствуеют обязательные аргументы');
+        }
+
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn('Уже присуствует звонок на это же время');
+        }
+
+        const newAlarm = {
+            time,
+            callback,
+            canCall: true
+        };
+        this.alarmCollection.push(newAlarm);
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time != time);
+    }
+
+    getCurrentFormattedTime() {
+        const now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+    }
+
+    start() {
+        if (this.inervalid) {
+            return;
+        }
+
+        this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === currentTime && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 100);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+
+        this.inervalid = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => {
+            alarm.cancel = true;
+        });
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+
+}
+
+function testCase() {
+    const clock = new AlarmClock();
+    const cb = function (time) {
+        console.log('alarm item ' + time);
+    }
+}
