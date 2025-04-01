6eef7b9a87a8140daf6844cd6e37891da1a9f2b0
SergozBy <sergozby@gmail.com>
2025-04-01 21:43:35 +0300

    Implementation

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    return arr1.every((value, index) => {
+        console.log(value, arr2[index]);
+        return value === arr2[index];
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+      return 0;
+  }
+
+  const age = filteredUsers.map(user => user.age);
+  const averageAge = (ages.reduce((sum, age) => (sum + age)))/filteredUsers.length;
+
+  return averageAge;
 }
