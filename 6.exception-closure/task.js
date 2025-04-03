d79f7fd6d5d67f030b70b84e2e0a1e4ca7af8f14
SergozBy <sergozby@gmail.com>
2025-04-03 17:16:17 +0300

    Fix: add cod for second task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -20,5 +20,52 @@ function validateCount(value) {
         return error;
     }
 }
+
 const invalidValue = validateCount('string');
 console.log(invalidValue.message);
+
+class Triangle {
+    constructor(a, b, c) {
+        if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return +area.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (e) {
+        console.log(e);
+
+        return {
+            get perimetr() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}
+
+const triangle = getTriangle(3, 4, 5);
+
+console.log(triangle.perimetr);
+console.log(triangle.area);
+
+const invalidTriangle = getTriangle(1, 2, 3);
